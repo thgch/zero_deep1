67e47e354850eadddfd6041f356a15a2743e9bcc
Tomohide Higuchi <tomohide.higuchi@gmail.com>
2020-05-17 12:25:46 +0900

    Completion of Chapter 1

--- ch1.py
+++ ch1.py
@@ -5,6 +5,9 @@ Auther: thgch
 Created on 2020/05/17
 
 """
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
 
 
 class Man:
@@ -19,5 +22,34 @@ class Man:
         print("Good-bye {}!".format(self.name))
 
 
+def draw_graph1():
+    x = np.arange(0, 6, 0.1)
+    y = np.sin(x)
+    plt.plot(x, y)
+    plt.show()
+
+
+def draw_graph2():
+    x = np.arange(0, 6, 0.1)
+    y1 = np.sin(x)
+    y2 = np.cos(x)
+    plt.plot(x, y1, label="sin")
+    plt.plot(x, y2, linestyle="--", label="cos")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title('sin & cos')
+    plt.legend()
+    plt.show()
+
+
+def draw_img():
+    img = imread('lena.png')
+    plt.imshow(img)
+    plt.show()
+
+
 if __name__ == '__main__':
-    pass
+    draw_graph1()
+    draw_graph2()
+    draw_img()
+
